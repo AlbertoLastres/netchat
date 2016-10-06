02a3b435a8ad1d70c696981438f3f085467baa4f
Alberto <albertok@me.com>
2016-10-06 20:21:50 +0200

    Init

---
+++ skel.cpp
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+	//Preguntar al usuario establecer parametros de inicio
+	//si dice que no: Crear usuario local con permisos para mensajeria.
+	
+
+
+
+
+	return EXIT_SUCCESS;
+}
